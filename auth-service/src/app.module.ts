9290ce881f0aa7783d37c18d97ea6658331c00ac
mwbintang <bintangmuhammadwahid@gmail.com>
2025-12-17 00:47:04 +0700

    feat(auth): add auth service as internal module

---
+++ auth-service/src/app.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common'
+import { GraphQLModule } from '@nestjs/graphql'
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo'
+import { join } from 'path'
+import { GraphqlHealthModule } from './graphql-health/graphql-health.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './auth/auth.module';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      playground: true,
+      sortSchema: true,
+    }),
+    GraphqlHealthModule,
+    PrismaModule,
+    AuthModule,
+  ],
+})
+export class AppModule {}
